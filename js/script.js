d0fb11b92bda15466be3ecf8688d14b9beedab4b
roshanrajeev <roshanrajeev2001@gmail.com>
2022-05-10 00:07:51 +0530

    Menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-const menuIcon = document.querySelector('.menu-icon')
+function toggleMenu() {
+    const menu = document.querySelector(".menu")
+    if(menu.classList.contains('active')) {
+        menu.classList.remove('active')
+    }else {
+        menu.classList.add('active')
+    }
+}
+
+document.body.addEventListener('click', (e) => {
+    if(e.target.classList.contains("menu-icon-container") ||
+    e.target.classList.contains("menu-icon")) {
+        toggleMenu()
+    }
+})
+
+
