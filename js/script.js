0b2afb8fe2848441291631a4f2e86a6dcd9aef80
roshanrajeev <roshanrajeev2001@gmail.com>
2022-05-10 03:46:42 +0530

    Tagline

--- js/script.js
+++ js/script.js
@@ -15,3 +15,19 @@ document.body.addEventListener('click', (e) => {
 })
 
 
+const textsContainerElem = document.querySelector(".tagline .texts")
+const textsElem = Array.from(textsContainerElem.children)
+const img = document.querySelector(".tagline .image img")
+console.log(textsContainerElem)
+textsContainerElem.addEventListener('mouseover', async (e) => {
+    if(e.target.classList.contains('text')) {
+        const textElem = e.target
+        const id = textElem.dataset.id
+        img.src = `./img/events/${id}.jpg`
+        
+    }
+})
+
+textsContainerElem.addEventListener('mouseleave', async (e) => {
+    img.src = `./img/collage2.jpg`
+})
