89fcf9229cc66b0cb6e546e22d7b9f9dc9132506
roshanrajeev <roshanrajeev2001@gmail.com>
2022-05-18 15:37:59 +0530

    Events

--- js/script.js
+++ js/script.js
@@ -23,33 +23,6 @@ document.body.addEventListener('click', (e) => {
 })
 
 
-const textsContainerElem = document.querySelector(".tagline .texts")
-const textsElem = Array.from(textsContainerElem.children)
-const img = document.querySelector(".tagline .image img")
-console.log(textsContainerElem)
-textsContainerElem.addEventListener('mouseover', async (e) => {
-    if(e.target.classList.contains('text')) {
-        const textElem = e.target
-        const id = textElem.dataset.id
-        img.src = `./img/events/${id}.jpg`
-        textElem.classList.add("active")
-        textsElem.forEach(e => {
-            e.classList.remove("animate")
-        })
-        
-    }
-})
-
-textsContainerElem.addEventListener('mouseleave', async (e) => {
-    img.src = `./img/collage2.jpg`
-    textsElem.forEach(e => e.classList.add("animate"))
-
-})
-
-textsContainerElem.addEventListener('mouseout', async (e) => {
-    textsElem.forEach(e => e.classList.remove("active"))
-})
-
 window.addEventListener("load", () => {
     setTimeout(() => {
         document.body.style.height = 'auto';
